002d0982cd49596990a7ba836967ec60f93aa038
Philip Patton <ppatton35745@gmail.com>
2018-06-25 11:28:29 -0500

    base project

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,8 @@
 $(document).ready(function () {
 
-    // Use jQuery to get a reference to `load-songs`
-
-    // Use jQuery to get a reference to `song-list`
-
-    /*
-        Attach a click handler to the button with jQuery. When
-        the button is clicked, use $.ajax() to load `songs.json`
-        from the file system
-    */
-
     $("#load-songs").on("click", function (e) {
         $.ajax("songs.json")
             .then(response => {
-
                 //songObj = JSON.parse(response);
                 response.songs.forEach(element => {
                     //const section = $("<section></section>").addClass("song");
@@ -24,12 +13,16 @@ $(document).ready(function () {
                                 .text(`Performed by ${element.artist} on the album ${element.album}`)
                             )
                         )
-                    //console.log(section);
-                    //$("#song-list").append(txt1, txt2, txt3)
                 });
             })
     })
-
-    // sourceComponent.on("click", function (evt) {
-    //     console.log(evt.target.id)
 })
+
+// Practice: Product Ordering
+// Your task is to simulate a product listing page like you would find on Etsy, Amazon, or Ebay. This application should implement the following features.
+
+// Display an individual product component. Each product should have a title, short description, price, quantity, and an image.
+// Display the application information in a navigation bar at the top of the page.
+// The name of the company - Betsy
+// Three links: Categories, Orders, and Log Out (_these links won't actually do anything, you're just practicing making the navigation bar a component)
+// Display a review list component that contains a maximum of 3 product review components beneath a product. You can use hipster ipsum for the reviews themselves.
